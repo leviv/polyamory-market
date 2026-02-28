625c215a985c6465f572e212c6b73b5c8fc15803
Levi Villarreal <villarreallevi@gmail.com>
2026-02-28 18:58:21 -0500

    hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import { Markets } from './pages/Markets'
 import { Market } from './pages/Market'
 import { Navbar } from './components/Navbar'
 
 function App() {
   return (
-    <BrowserRouter basename="/polyamory-market">
+    <HashRouter>
       <Navbar />
 
       <Routes>
         <Route path="/" element={<Markets />} />
         <Route path="/:marketName" element={<Market />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
